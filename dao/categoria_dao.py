e1e103de65bb6cf2e2f46672967a99ccb5ff56ea
Jonilson Sousa <133526977+jonilsonds16@users.noreply.github.com>
2023-12-07 00:33:03 +0000

    Removendo categoria do banco de dados

--- dao/categoria_dao.py
+++ dao/categoria_dao.py
@@ -36,15 +36,17 @@ class CategoriaDAO:
         conexao.close()
 
     def remover(self, categoria_id: int) -> bool:
-        encontrado = False
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute("DELETE FROM categorias WHERE id = 10", (categoria_id,))
+        categorias_removidas = cursor.rowcount
+        conexao.commit()
+        cursor.close()
+        conexao.close()
 
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                index = self.__categorias.index(c)
-                self.__categorias.pop(index)
-                encontrado = True
-                break
-        return encontrado
+        if (categorias_removidas == 0):
+            return False
+        return True
 
     def buscar_por_id(self, categoria_id) -> Categoria:
         cat = None
